19f279b67393600604782b96e4dfcb94f0c38a71
sainivi931 <sainivi931@gmail.com>
2025-04-05 00:46:39 +0530

    Create Student

--- controllers/studentControllers.js
+++ controllers/studentControllers.js
@@ -2,64 +2,97 @@ const db = require("../Config/db");
 
 // GET ALL STUDENTS LIST
 const getStudents = async (req, res) => {
-   try {
-    const [records] = await db.query( 'SELECT * FROM student' )
-    if(!records){
-        return res.status(404).send({
-            success : false,
-            message: 'No Records Found'
-        })
-    } 
+  try {
+    const [records] = await db.query("SELECT * FROM student");
+    if (!records) {
+      return res.status(404).send({
+        success: false,
+        message: "No Records Found",
+      });
+    }
     res.status(200).send({
-        success: true,
-        message: 'All Student Records',
-        totalStudents: records.length,
-        records : records[0]
-    })
-   } catch (error) {
+      success: true,
+      message: "All Student Records",
+      totalStudents: records.length,
+      records: records[0],
+    });
+  } catch (error) {
     console.log(error);
     res.status(500).send({
-        success: false,
-        message: 'Error in  Get All Student Api',
-        error
-    })
-    
-   }
-}
+      success: false,
+      message: "Error in  Get All Student Api",
+      error,
+    });
+  }
+};
 
-// GET STUDENT BY ID 
+// GET STUDENT BY ID
 
-const getStudentByid = async (req,res) =>{
-try {
-    const studentId = req.params.id
-    if(!studentId){
-        return res.status(404).send({
-            success: true,
-            message: 'Invalid or Provide student id'
-
-        })
+const getStudentByid = async (req, res) => {
+  try {
+    const studentId = req.params.id;
+    if (!studentId) {
+      return res.status(404).send({
+        success: true,
+        message: "Invalid or Provide student id",
+      });
     }
     //  const data = await db.query(`SELECT FROM Student WHERE id =`,+ studentId
-    const data = await db.query(`SELECT * FROM student WHERE id = ?`,[studentId])
+    const data = await db.query(`SELECT * FROM student WHERE id = ?`, [
+      studentId,
+    ]);
     if (!data) {
-        return res.status(404).send({
-            success: false,
-            message: 'Records not found'
-        })
+      return res.status(404).send({
+        success: false,
+        message: "Records not found",
+      });
     }
     res.status(200).send({
-        success: true,
-        studentDeatils : data[0],
-    })
-} catch (error) {
+      success: true,
+      studentDeatils: data[0],
+    });
+  } catch (error) {
     console.log(error);
     res.status(500).send({
-        success: false,
-        message: 'Error in  Get All Student by id Api ',
-        error
-    })
-    
-}
-}
+      success: false,
+      message: "Error in  Get All Student by id Api ",
+      error,
+    });
+  }
+};
 
-module.exports = { getStudents, getStudentByid }
+// Create Student
+const createStudent = async (req, res) => {
+  try {
+    const { roll_no,standerd, fees, medum } = req.body;
+    if (!roll_no || !standerd || !fees || !medum) {
+      return res.status(500).send({
+        success: false,
+        message: "Please provide all Fealds",
+      });
+    }
+    const data = await db.query(
+      `INSERT INTO student (roll_no,standerd,fees,medum) VALUES (?, ?, ?, ?)`,
+      [roll_no, standerd, fees, medum]
+    );
+    if(!data){
+        return res.status(404).send({
+            success: false,
+            message: 'Error in INSERT QUERY'
+        })
+    }
+    res.status(201).send({
+        success: true,
+        message: 'New Student Record Created'
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in Create student API",
+      error,
+    });
+  }
+};
+
+module.exports = { getStudents, getStudentByid, createStudent };
--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,16 +1,22 @@
-const express = require('express');
-const { getStudents, getStudentByid } = require('../controllers/studentControllers');
+const express = require("express");
+const {
+  getStudents,
+  getStudentByid,
+  createStudent,
+} = require("../controllers/studentControllers");
 
 // router object
 
-const router = express.Router()
+const router = express.Router();
 
 // GET All STUDENTS LIST || GET
 
-router.get('/getall',getStudents)
+router.get("/getall", getStudents);
 
 // GET STUDENT BY ID
-router.get('/get/:id', getStudentByid)
+router.get("/get/:id", getStudentByid);
 
+// Create Student || POST
+router.post("/create", createStudent);
 
-module.exports = router
+module.exports = router;
